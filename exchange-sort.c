92e232edf1bb56d15470f1fe3b87ab9dd11d1d07
Arjun <86298279+ArjunGTX@users.noreply.github.com>
2021-12-14 14:40:35 +0530

    exchange sort

---
+++ exchange-sort.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main(void)
+{
+    int array[5], i, j, temp,n;
+    printf("Enter the number of elements : ");
+    scanf("%d",&n);
+    printf("Enter %d numbers : \n",n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d",&array[i]);
+    }
+    printf("\nthe original array is ");
+    for(i=0;i<n; i++) {
+        printf(" %d ",array[i]);
+    }
+    for(i = 0; i < (n -1); i++)
+    {
+        for (j=(i + 1); j < n; j++)
+        {
+            if (array[i] > array[j])
+            {
+                temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+    printf("\nSorted array is : ");
+    for (i = 0; i < n; i++)
+    {
+        printf(" %d ",array[i]);
+    }
+    return 0;
+}
